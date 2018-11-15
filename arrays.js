4c647e262b919524726d821b7e0406c15564b1f7
Ted Sazon <tedsazon@gmail.com>
2018-11-15 08:14:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,11 +4,11 @@ function addElementToBeginningOfArray(array, element) {
   return addElementToBeginningOfArray.unshift
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return (element, array)
+  return (array, element...)
 }
 function addElementToEndOfArray(array, element) {
   return addElementToEndOfArray.push
 }
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  return (element, array)
+function destructivelyAddElementToEndOfArray(array, element) {
+  return (array, )
 }
