bf37bdff9ec8cabbac2fb8897b919ece4d7a49a5
Ted Sazon <tedsazon@gmail.com>
2018-11-15 09:30:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]
   var candyString = 'foo'
-var addElementToBeginningOfArray = 
-function addElementToBeginningOfArray(chocolateBars) {
-  return addElementToBeginningOfArray.concat
-  
 
-function destructivelyAddElementToBeginningOfArray(1, 'foo') {
-  return destructivelyAddElementToBeginningOfArray.unshift
-}
-function addElementToEndOfArray(array, element) {
-  return addElementToEndOfArray.push
-}
-function destructivelyAddElementToEndOfArray(array, element) {
-  return destructivelyAddElementToEndOfArray.shift
-}
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString, ...chocolateBars]
