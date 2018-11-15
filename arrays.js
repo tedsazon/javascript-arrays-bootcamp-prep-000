cb61224402d108e67bfb3d02127431065d72a032
Ted Sazon <tedsazon@gmail.com>
2018-11-15 09:08:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]
 
 function addElementToBeginningOfArray(array,element) {
-  return addElementToBeginningOfArray.unshift
+  return addElementToBeginningOfArray.unshift {
+    return ([1], 'foo')
+  }
 }
