8955dc9ce19080c7eeb71fdc35be2e93703457e1
Ted Sazon <tedsazon@gmail.com>
2018-11-15 07:25:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,1 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-
-function addElementToBeginningOfArray(array, element) {
-  return 1, for (variable of iterable) {
-
-  }
-}
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles" ]
